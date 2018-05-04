8c4216366944ca313d2a91940c64117e73c861fc
DESKTOP-2NLB9QJ\Krkt <mustafasacli70@gmail.com>
2018-05-04 17:02:00 +0300

    Kodda düzenlemeler yapıldı. Test Console uygulaması eklendi. DynamicExtensions eklendi. Build Output Path düzenlendi. İmzalama işleri yapıldı. Public Key Token eklendi.

---
+++ DexterSolution/Dexter.Extensions/DynamicExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+namespace Dexter.Extensions
+{
+    public static class DynamicExtensions
+    {
+        public static T ConvertTo<T>(this ExpandoObject dyn) where T : class
+        {
+            if (dyn == null)
+            {
+                throw new ArgumentNullException(nameof(dyn));
+            }
+
+            T tt = Activator.CreateInstance<T>();
+
+            IDictionary<string, object> dict = dyn;
+
+            var props = typeof(T).GetProperties();
+
+            props = props.AsQueryable().Where(p => p.CanWrite == true).ToArray();
+            props = props.AsQueryable().Where(p => dict.ContainsKey(p.Name) == true).ToArray();
+
+            foreach (var prp in props)
+            {
+                prp.SetValue(tt, dict[prp.Name]);
+            }
+
+            return tt;
+        }
+
+    }
+}
---
+++ DexterSolution/Dexter.TestConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dexter;
+using Dexter.Factory;
+using Dexter.Extensions;
+
+namespace Dexter.TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sConnType = ConfigurationManager.AppSettings["connType"];
+            sConnType = sConnType ?? "mysql";
+            string sConnStr = ConfigurationManager.AppSettings["connString"];
+            sConnStr = sConnStr ?? "Server=127.0.0.1;database=mst_stock;Uid=root;Pwd=my123123;";
+
+            IDbConnection conn = ConnectionFactory.Instance.GetConnection(sConnType);//"mysql");
+            conn.ConnectionString = sConnStr;
+
+            conn.Open();
+            Console.WriteLine("Connection opened.");
+            object obj = conn.ExecuteScalar("SELECT COUNT(1) FROM stock;");
+            Console.WriteLine($"Result is {obj}.");
+            List<dynamic> list = conn.GetDynamicResultSet(sql: "SELECT *, 1 AS STOCK_ID FROM stock;");
+            conn.Close();
+            Console.WriteLine("Connection closed.");
+            //List<dynamic> list = conn.ExecuteReader("SELECT *, 1 AS STOCK_ID FROM stock;").GetDbReaderResultV2();
+            IDictionary<string, object> dict;
+            Console.WriteLine("**************************");
+            foreach (var item in list)
+            {
+                dict = item;
+
+                foreach (var key in dict.Keys)
+                {
+                    Console.WriteLine($"{key} : {dict[key]}");
+                }
+                Console.WriteLine("---------------------------");
+            }
+            Console.WriteLine("**************************");
+            Console.ReadKey();
+        }
+    }
+}
